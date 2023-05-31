d90febb609e34a0449fdaaba5295873dc0248e84
Kevin9614 <134111336+Kevin9614@users.noreply.github.com>
2023-05-30 20:35:46 -0500

    Initial commit

---
+++ estilo.css
@@ -0,0 +1,32 @@
+.container-nav {
+    display: flex;
+    padding: 10px;
+    height: 50px;
+    background-color: black;
+    }
+    .navbar {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    }
+    .brand {
+    display: flex;
+    align-items: center;
+    }
+    li>a {
+    padding: 8px 24px;
+    }
+    li {
+    list-style-type: none;
+    }
+    a {
+    color: white;
+    text-decoration:none;
+    }
+    .nav-links {
+    display: flex;
+    align-items: center;
+    }
+    .button-menu-toggle {
+    display: none;
+    }
